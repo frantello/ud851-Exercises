11ce2544ea331421ee7e21ea20322d8152bb96a8
Fran Tello <jfrantello@gmail.com>
2017-02-10 19:26:58 +0100

    Build our URL

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,16 +15,26 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
+
+
+import static java.net.URI.create;
 
 /**
  * These utilities will be used to communicate with the network.
  */
 public class NetworkUtils {
+
+    private static final String TAG = "NetworkUtils";
 
     final static String GITHUB_BASE_URL =
             "https://api.github.com/search/repositories";
@@ -39,14 +49,29 @@ public class NetworkUtils {
     final static String sortBy = "stars";
 
     /**
-     * Builds the URL used to query Github.
+     * Builds the URL used to query GitHub.
      *
-     * @param githubSearchQuery The keyword that will be queried for.
-     * @return The URL to use to query the weather server.
+     * @param gitHubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the GitHub's repositories.
      */
-    public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+    public static URL buildUrl(String gitHubSearchQuery) {
+
+        // COMPLETED (1) Fill in this method to build the proper GitHub query URL
+        Uri uri =  Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, gitHubSearchQuery)
+                .build();
+
+        URL url = null;
+        try {
+
+            url = new URL(uri.toString());
+
+        } catch (MalformedURLException e) {
+
+            Log.e(TAG, "Malformed URL", e);
+        }
+
+        return url;
     }
 
     /**
